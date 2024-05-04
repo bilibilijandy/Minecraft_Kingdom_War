44527af59e0641738a8498e1e0313c74a52d8c42
bilibilijandy <141329748+bilibilijandy@users.noreply.github.com>
2024-05-04 11:48:07 +0800

    v0.2.8.1更新 * 修复了 bStats 的问题 * 不在使用System.out.println方法

--- src/main/java/me/jandy/minecraft_kingdom_war/SQL/SQLite.java
+++ src/main/java/me/jandy/minecraft_kingdom_war/SQL/SQLite.java
@@ -1,5 +1,8 @@
 package me.jandy.minecraft_kingdom_war.SQL;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,13 +10,18 @@ import java.sql.SQLException;
 public class SQLite {
     private static Connection connection;
 
+    public static void say(String s){
+        CommandSender sender = Bukkit.getConsoleSender();
+        sender.sendMessage("[王国战争]"+s);
+    }
+
     public static void connectToDatabase() {
         try {
             String url = "jdbc:sqlite:/data.db";
             connection = DriverManager.getConnection(url);
-            System.out.println("成功连接到 SQLite 数据库");
+            say("成功连接到 SQLite 数据库");
         } catch (SQLException e) {
-            System.out.println("连接到 SQLite 数据库失败");
+            say("连接到 SQLite 数据库失败");
             e.printStackTrace();
         }
     }
@@ -22,9 +30,9 @@ public class SQLite {
         if (connection != null) {
             try {
                 connection.close();
-                System.out.println("已断开与 SQLite 数据库的连接");
+                say("已断开与 SQLite 数据库的连接");
             } catch (SQLException e) {
-                System.out.println("关闭数据库连接时出现错误");
+                say("关闭数据库连接时出现错误");
                 e.printStackTrace();
             }
         }
