f98ddb50243db915ff03fde6f0ed1c3962c3f15e
bilibilijandy <141329748+bilibilijandy@users.noreply.github.com>
2024-05-04 10:53:44 +0800

    v0.2.8更新 * 修复了 bStats 的问题

--- src/main/java/me/jandy/minecraft_kingdom_war/Minecraft_Kingdom_War.java
+++ src/main/java/me/jandy/minecraft_kingdom_war/Minecraft_Kingdom_War.java
@@ -6,8 +6,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-//import org.bstats.bukkit.Metrics;
-import me.jandy.minecraft_kingdom_war.bStats.Metrics;
+import org.bstats.bukkit.Metrics;
+//import me.jandy.minecraft_kingdom_war.bStats.Metrics;
 import me.clip.placeholderapi.PlaceholderAPI;
 
 import me.jandy.minecraft_kingdom_war.SQL.SQLite;
@@ -62,7 +62,7 @@ public final class Minecraft_Kingdom_War extends JavaPlugin implements CommandEx
             }
 
             else if (args[0].equals("add")) {
-                if(args.length < 1){
+                if(args.length < 3){
                     help(sender);
                 }
                 else{
